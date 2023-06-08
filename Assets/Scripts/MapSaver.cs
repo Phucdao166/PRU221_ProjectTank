85e29ef32b77dec4d564049d5fd12eec3285f936
Đào Xuân Phúc <108455970+Phucdao166@users.noreply.github.com>
2023-06-08 17:14:40 +0700

    fix

---
+++ Assets/Scripts/MapSaver.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MapSaver : MonoBehaviour
+{
+	public Button saveButton;
+	public Scene previousScene;
+
+	private void Start()
+	{
+		saveButton = GetComponent<Button>();
+		saveButton.onClick.AddListener(SaveScene);
+	}
+
+	public void SaveScene()
+	{
+		previousScene = SceneManager.GetActiveScene();
+
+		GameObject[] sceneObjects = previousScene.GetRootGameObjects();
+		GameObject[] cloneObjects = new GameObject[sceneObjects.Length];
+		for (int i = 0; i < sceneObjects.Length; i++)
+		{
+			cloneObjects[i] = Instantiate(sceneObjects[i]);
+			DontDestroyOnLoad(cloneObjects[i]);
+		}
+
+		SceneManager.LoadScene("NewScene");
+
+		for (int i = 0; i < cloneObjects.Length; i++)
+		{
+			Destroy(cloneObjects[i]);
+		}
+	}
+}
